366999a438288f1cdd74408dae063481e7511457
TheRazvy27 <razvannicks@yahoo.com>
2024-07-05 11:36:05 +0300

    functionalitate totala

--- batalie.py
+++ batalie.py
@@ -4,18 +4,28 @@ class batalie:
             pokemon1 = antrenor1.alege_pokemon()
             pokemon2 = antrenor2.alege_pokemon()
             print(pokemon1.nume, " vs ",pokemon2.nume)
-            print ("Pokemon1: ", pokemon1.viata)
-            print ("Pokemon2: ", pokemon2.viata)            
+            print ("Pokemon1: ", pokemon1.nume, " ", pokemon1.viata)
+            print ("Pokemon2: ", pokemon2.nume, " ", pokemon2.viata)          
             pokemon1.ataca(pokemon2)
-            print ("Pokemon1: ", pokemon1.viata)
-            print ("Pokemon2: ", pokemon2.viata)
+            print ("Pokemon1: ", pokemon1.nume, " ", pokemon1.viata)
+            print ("Pokemon2: ", pokemon2.nume, " ", pokemon2.viata)
             if not pokemon2.este_viu():
-                break    
+                antrenor2.pokemoni.pop(0) 
+                if not antrenor2.are_pokemoni_vii():
+                    print("antrenor 2 nu mai are pokemoni")
+                    break
+                pokemon2 = antrenor2.alege_pokemon() 
+                  
             pokemon2.ataca(pokemon1)
-            print ("Pokemon1: ", pokemon1.viata)
-            print ("Pokemon2: ", pokemon2.viata)
+            print ("Pokemon1: ", pokemon1.nume, " ", pokemon1.viata)
+            print ("Pokemon2: ", pokemon2.nume, " ", pokemon2.viata)
             if not pokemon1.este_viu():
-                break        
+                antrenor1.pokemoni.pop(0)
+                if not antrenor1.are_pokemoni_vii():
+                    print("antrenor 1 nu mai are pokemoni")
+                    break
+                pokemon1 = antrenor1.alege_pokemon()   
+                     
 
     
     def test():
